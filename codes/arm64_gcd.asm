e50530c743750c153fc871c4b85d3617e8c25d51
Siyao (Jane) He <siyaoh4@uci.edu>
2023-04-02 12:57:40 -0700

    Update arm64_gcd.asm

--- codes/arm64_gcd.asm
+++ codes/arm64_gcd.asm
@@ -9,30 +9,30 @@ ASM_START()
 	ALIGN(16)
 PROLOGUE(mpn_gcd_11)
 	subs	x3, u0, v0		
-	b.eq	L(end)				; 			jump to end if u == v
+	b.eq	L(end)				; jump to end if u == v
 
 ALIGN(16)
 	L(top):	
 		; x12 = # of trailing zeros of (u-v)
 
-		rbit	x12, x3				;			reverse bit order
-		clz		x12, x12			;			count leading zeros
+		rbit	x12, x3				; reverse bit order
+		clz		x12, x12			; count leading zeros
 
 		; if u >= v
 		; 	x3 = u-v
 		; else
 		;	x3 = -(u-v)
 
-		csneg	x3, x3, x3, cs		;  			v = abs(u-v)
+		csneg	x3, x3, x3, cs		; v = abs(u-v)
 
 		; u = u >= v ? v : u
 
-		csel	u0, v0, u0, cs		; 			u = min(u,v)		
+		csel	u0, v0, u0, cs		; u = min(u,v)		
 
 		; discard trailing zeros
-		lsr		v0, x3, x12			;			logical shift right
+		lsr		v0, x3, x12			; logical shift right
 		subs	x3, u0, v0			
-		b.ne	L(top)				; 			if u0 != v0, loop
+		b.ne	L(top)				; if u0 != v0, loop
 
 	L(end):	ret
 		EPILOGUE()
