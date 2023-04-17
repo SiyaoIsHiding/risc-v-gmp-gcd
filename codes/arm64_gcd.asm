e31cbbf74dcb281a8447c56bbd42abde37dab4a9
siyao <siyaoh4@uci.edu>
2023-04-17 11:46:48 -0700

    formatting

--- codes/arm64_gcd.asm
+++ codes/arm64_gcd.asm
@@ -3,36 +3,37 @@ include(`../config.m4')
 define(`u0',    `x0')
 define(`v0',    `x1')
 
-	; calculate the gcd when both parameters are odd
+# calculate the gcd when both parameters are odd
 ASM_START()
-	TEXT
-	ALIGN(16)
+    TEXT
+    ALIGN(16)
 PROLOGUE(mpn_gcd_11)
-	subs	x3, u0, v0		
-	b.eq	L(end)					; jump to end if u == v
+    subs	x3, u0, v0
+    b.eq	L(end)					# jump to end if u == v
 
 ALIGN(16)
-	L(top):	
-		; x12 = # of trailing zeros of (u-v)
+    L(top):
 
-		rbit	x12, x3				; reverse bit order
-		clz	x12, x12				; count leading zeros
+# x12 = # of trailing zeros of (u-v)
 
-		; if u >= v
-		; 	x3 = u-v
-		; else
-		;	x3 = -(u-v)
+        rbit	x12, x3				# reverse bit order
+        clz	    x12, x12			# count leading zeros
 
-		csneg	x3, x3, x3, cs			; v = abs(u-v)
+# if u >= v
+# 	x3 = u-v
+# else
+#	x3 = -(u-v)
 
-		; u = u >= v ? v : u
+        csneg	x3, x3, x3, cs		# v = abs(u-v)
 
-		csel	u0, v0, u0, cs			; u = min(u,v)		
+# u = u >= v ? v : u
 
-		; discard trailing zeros
-		lsr	v0, x3, x12				; logical shift right
-		subs	x3, u0, v0			
-		b.ne	L(top)				; if u0 != v0, loop
+        csel	u0, v0, u0, cs		# u = min(u,v)
 
-	L(end):	ret
-		EPILOGUE()
+# discard trailing zeros
+        lsr	    v0, x3, x12			# logical shift right
+        subs	x3, u0, v0
+        b.ne	L(top)				# if u0 != v0, loop
+
+    L(end):	ret
+EPILOGUE()
--- codes/risc_double_conversion.asm
+++ codes/risc_double_conversion.asm
@@ -1,36 +1,36 @@
 include(`../config.m4')
 
 
-# 		a0 for u, also the count of trailing zeros of u 
+# 		a0 for u, also the count of trailing zeros of u
 # 		a1 for v
 #  		a2 for the high bit mask
 # 		a3 for intermediary value for u, before shifting
 #  		a4 for t = u - v
-# 		a5 for intermediary value when counting trailing zeros and also the final count'
+# 		a5 for intermediary value when counting trailing zeros and also the final count
 
 ASM_START()
 
 PROLOGUE(mpn_gcd_11)
-# u >>= 1;
-        srli    a0,a0,1 #, u
-# v >>= 1;
-        srli    a1,a1,1 #, v
+# u >>= 1
+        srli    a0,a0,1
+# v >>= 1
+        srli    a1,a1,1
 # while (u != v)
-        beq     a0,a1,L(end)       #, u, v,
-          
+        beq     a0,a1,L(end)
+
 L(top):
-# t = u - v;
-        sub     a4,a0,a1        # t, u, v
+# t = u - v
+        sub     a4,a0,a1
 # a2 = mask of t (vgtu)
-        srai    a2,a4,63        
-# the `&` in v += (vgtu & t);
-        and     a7,a4,a2    
-# the `^` in u = (t ^ vgtu) - vgtu;
-        xor     a3,a4,a2        
+        srai    a2,a4,63
+# the `&` in v += (vgtu & t)
+        and     a7,a4,a2
+# the `^` in u = (t ^ vgtu) - vgtu
+        xor     a3,a4,a2
 
 # the `-` in a3 = (t ^ vgtu) - vgtu
-        sub     a3,a3,a2        
-# the `+=` in v += (vgtu & t);
+        sub     a3,a3,a2
+# the `+=` in v += (vgtu & t)
         add     a1,a1,a7
 
 # count trailing zeros
@@ -41,19 +41,19 @@ L(top):
         srli 	a5, a5, 52 		# discard the fraction
         addi 	a5, a5, -1022 	# 1023 is the bias, 1 is the 1 in (u >> 1) >> c
 
-# u = (u >> 1) >> c;
-        srl     a0,a3,a5        
+# u = (u >> 1) >> c
+        srl     a0,a3,a5
 # while (u != v) loop
-        bne     a1,a0,L(top)    
+        bne     a1,a0,L(top)
 L(end):
-# return (u << 1) + 1;
-        slli    a0,a0,1 
-        addi    a0,a0,1 
+# return (u << 1) + 1
+        slli    a0,a0,1
+        addi    a0,a0,1
         ret
 
 
-  
-       
+
+
 
 EPILOGUE()
 ASM_END()
